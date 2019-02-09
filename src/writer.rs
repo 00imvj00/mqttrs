6328e18bbd802e885359dd84a454c3e88fa8bd9c
00imvj00 <vijaybambhaniya007@gmail.com>
2019-02-09 11:50:13 +0530

    more code to encode decode added

--- src/writer.rs
+++ src/writer.rs
@@ -1,3 +1,12 @@
+use crate::Packet;
+use std::io;
 use std::io::Write;
 
-pub trait MqttWriter: Write {}
+pub trait MqttWriter: Write {
+    fn write_packet(&mut self, _packet: Packet) -> Result<(), io::Error> {
+        //TODO: ENCODE PACKET TO BYTES ARRAY
+        let data = [];
+        let _ = self.write(&data);
+        Ok(())
+    }
+}
