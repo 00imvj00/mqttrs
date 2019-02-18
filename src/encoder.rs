0bbecb5c149a724e856576571f1d1b0bfd5fa4da
00imvj00 <vijaybambhaniya007@gmail.com>
2019-02-18 14:38:34 +0530

    git ignore updated for vim and encoder wrapper added.

--- src/encoder.rs
+++ src/encoder.rs
@@ -1,6 +1,23 @@
 use crate::Packet;
 use std::io;
+use bytes::BytesMut;
 
-pub fn encode(packet: &Packet, buffer: &mut Vec<u8>) -> Result<(), io::Error> {
-    unimplemented!()
+pub fn encode(packet: &Packet, buffer: &mut BytesMut) -> Result<(), io::Error> {
+    match packet {
+        Packet::Connect(connect) => Ok(()), 
+        Packet::Connack(connack) => Ok(()),
+        Packet::Publish(publish) => Ok(()),
+        Packet::Puback(pid) => Ok(()),
+        Packet::Pubrec(pid) => Ok(()),
+        Packet::Pubrel(pid) => Ok(()),
+        Packet::PubComp(pid) => Ok(()),
+        Packet::Subscribe(subscribe) => Ok(()),
+        Packet::SubAck(suback) => Ok(()),
+        Packet::UnSubscribe(unsub) => Ok(()),
+        Packet::UnSubAck(pid) => Ok(()),
+        Packet::PingReq => Ok(()),
+        Packet::PingResp => Ok(()),
+        Packet::Disconnect => Ok(()),
+        Packet::None => Ok(()),
+    }
 }
