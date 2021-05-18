78b1d2a93ff1e49354070242e38e73f85e7ee93f
Mathias <mk@blackbird.online>
2021-05-18 16:13:14 +0200

    Update heapless to v0.7.0 and use const generics

--- src/publish.rs
+++ src/publish.rs
@@ -1,9 +1,5 @@
 use crate::{decoder::*, encoder::*, *};
 
-// use alloc::{string::String, vec::Vec};
-use heapless::{String, Vec, consts};
-
-
 /// Publish packet ([MQTT 3.3]).
 ///
 /// [MQTT 3.3]: http://docs.oasis-open.org/mqtt/mqtt/v3.1.1/os/mqtt-v3.1.1-os.html#_Toc398718037
